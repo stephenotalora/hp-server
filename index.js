215eebbc7bc2943d3689833eae211e5d2fcf6882
Jonathan Otalora <j.otalora@me.com>
2016-10-17 22:44:32 -0700

    fixed errors with the server

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const config = require('./config');
-const {getRoutes} = requrie('./src/getRoutes');
-const {Server} = requrie('hapi');
+const {helloRoute} = require('./src/getRoutes');
+const {Server} = require('hapi');
 
 const server = new Server();
 server.connection(config);
 
-getRoutes(server);
+helloRoute(server);
 
 server.start(() => {
 	console.log(`Started at: ${server.info.uri}`);
